e9aa3f734de8bdbc3c32dd06af220d6d7650601e
fantadenaranja <fanta@hackingaround.net>
2013-10-17 02:04:16 +0200

    Titulos diferentes para cada sección

--- addnode.php
+++ addnode.php
@@ -79,7 +79,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" dir="ltr" lang="es">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title><?php echo "$title $version - $nick"; ?></title>
+	<title><?php echo "Follow the leader - $title $version - $nick"; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="asocial network" content="">
     <meta name="fanta" content="">
